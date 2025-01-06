d687c9d01e2548f417cd2db48147a6003909f810
codrinursachi <codrinursachi@gmail.com>
2025-01-06 22:18:55 +0200

    add first_name and last_name columns in user migration

--- db/migrate/20250106200913_create_users.rb
+++ db/migrate/20250106200913_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[8.0]
     create_table :users do |t|
       t.string :email_address, null: false
       t.string :password_digest, null: false
-
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.timestamps
     end
     add_index :users, :email_address, unique: true
