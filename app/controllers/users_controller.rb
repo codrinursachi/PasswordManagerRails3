3da44343af9f8ffd3a8f788d5ed53d7dbce5b82c
codrinursachi <codrinursachi@gmail.com>
2025-01-08 09:29:59 +0200

    add folders on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.folders << Folder.new(name: "No folder")
+    @user.folders << [Folder.new(name: "No folder"), Folder.new(name: "Email"), Folder.new(name: "Social media"), Folder.new(name: "Banking"), Folder.new(name: "Shopping"), Folder.new(name: "Work"), Folder.new(name: "Other")]
     # If the user is saved, log them in and redirect to the home page
     if @user.save
       start_new_session_for @user  
