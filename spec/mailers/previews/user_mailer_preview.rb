361d9bd277817330270e84fbaeb456089290f2dc
codrinursachi <codrinursachi@gmail.com>
2025-02-03 18:31:24 +0200

    run rails generate mailer User shared_login

---
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/shared_login
+  def shared_login
+    UserMailer.shared_login
+  end
+
+end
